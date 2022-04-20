6442cc32fabe9386ead46f5f2d725c604a361a3b
milan.vrozina@gmail.com <milan.vrozina@gmail.com>
2022-04-20 17:17:08 +0200

    1.0.0.1 - beta

--- getcert/Properties/AssemblyInfo.cs
+++ getcert/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("getcert")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("getcert utility")]
+[assembly: AssemblyDescription("Simple utility to get certificate (chain) from given url")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("getcert")]
-[assembly: AssemblyCopyright("Copyright © Milan Vrozina 2022")]
+[assembly: AssemblyCopyright("Copyright (c) Milan Vrozina 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
